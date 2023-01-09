6188144640282db4691cc3d7b970a8dea4d1cc13
Itunu Raimi <chavitunu@gmail.com>
2023-01-09 23:54:13 +0100

    include overlay for app in inactive state

--- Finish/Binding/AppDelegate.swift
+++ Finish/Binding/AppDelegate.swift
@@ -9,28 +9,30 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    // 1
+    var window: UIWindow?
+    let overlay = UIView()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        //2
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func applicationWillResignActive(_ application: UIApplication) {
+        // 3
+        if let window = window {
+            overlay.frame = window.frame
+            overlay.backgroundColor = .black.withAlphaComponent(0.95)
+            window.addSubview(overlay)
+        }
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // 4
+        overlay.removeFromSuperview()
     }
-
-
 }
 
